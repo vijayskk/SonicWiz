b120f0391b500b79984a3e1b6f03553f943c45aa
vijayskk <vijayskk007@gmail.com>
2025-09-03 10:28:06 +0530

    Basic Stream setup only with pulseaudio

---
+++ Makefile
@@ -0,0 +1,25 @@
+CC=g++
+ARGS= -I./lib/portaudio/include ./lib/portaudio/lib/.libs/libportaudio.a -lrt -lasound -ljack -pthread
+EXEC=build/run
+
+main:main.cpp
+	mkdir -p build
+	$(CC) -o $(EXEC) $^ $(ARGS)
+
+install-deps:
+	mkdir -p lib
+
+	curl https://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz | tar -zx -C lib
+	cd lib/portaudio && ./configure && $(MAKE) -j
+.PHONY: install-deps
+
+uninstall-deps:
+	cd lib/portaudio $$ $(MAKE) uninstall
+	rm -rf lib/portaudio
+.PHONY: uninstall-deps
+
+run:
+	./build/run 2>/dev/null
+
+clean:
+	rm build/*
---
+++ main.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <portaudio.h>
+#include <string>
+#include <cstring>
+#define SAMPLE_RATE 44100
+#define FRAMES_PER_BUFFER 512
+
+static void checkError(PaError err)
+{
+    if (err != paNoError)
+    {
+        printf("PORTAUDIO ERROR: %s\n", Pa_GetErrorText(err));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static int paTestCallback(const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags, void *userdata)
+{
+    float * in = (float *)inputBuffer;
+    (void)outputBuffer;
+
+    int displaySize = 100;
+    printf("\r");
+
+    float vol_l = 0;
+    float vol_r = 0;
+
+    fflush(stdout);
+}
+
+int main(int argc, char const *argv[])
+{
+    PaError error;
+    error = Pa_Initialize();
+    checkError(error);
+
+    int numDevices = Pa_GetDeviceCount();
+    printf("Number of Devices : %d\n\n", numDevices);
+    if (numDevices < 0)
+    {
+        printf("ERROR: Device count query failed.");
+        exit(EXIT_FAILURE);
+    }
+    else if (numDevices == 0)
+    {
+        printf("ERROR: No devices found.");
+        exit(EXIT_FAILURE);
+    }
+
+    PaDeviceInfo *deviceInfo;
+    int selectedDevice = 0;
+    for (int i = 0; i < numDevices; i++)
+    {
+        deviceInfo = (PaDeviceInfo *)Pa_GetDeviceInfo(i);
+        printf("Device: %d\n", i);
+        printf("\tName: %s\n", deviceInfo->name);
+        printf("\tMax Input Channels: %d\n", deviceInfo->maxInputChannels);
+        printf("\tMax Output Channels: %d\n", deviceInfo->maxOutputChannels);
+        printf("\tSample Rate: %f\n\n", deviceInfo->defaultSampleRate);
+        if (std::string(deviceInfo->name) == std::string("pulse"))
+        {
+            selectedDevice = i;
+            printf("Selected: %d", selectedDevice);
+        }
+    }
+    
+
+    PaStreamParameters inputParameters;
+    PaStreamParameters outputParameters;
+    memset(&inputParameters, 0, sizeof(inputParameters));
+    memset(&outputParameters, 0, sizeof(outputParameters));
+
+    inputParameters.channelCount = 2;
+    inputParameters.device = selectedDevice;
+    inputParameters.hostApiSpecificStreamInfo = NULL;
+    inputParameters.sampleFormat = paFloat32;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo(selectedDevice)->defaultLowInputLatency;
+
+    outputParameters.channelCount = 2;
+    outputParameters.device = selectedDevice;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+    outputParameters.sampleFormat = paFloat32;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo(selectedDevice)->defaultLowInputLatency;
+
+    PaStream *stream;
+    error = Pa_OpenStream(
+        &stream,
+        &inputParameters,
+        &outputParameters,
+        SAMPLE_RATE,
+        FRAMES_PER_BUFFER,
+        paNoFlag,
+        paTestCallback,
+        NULL
+    );
+    checkError(error);
+
+    error = Pa_StartStream(stream);
+    checkError(error);
+
+    Pa_Sleep(10*1000);
+
+    error = Pa_StopStream(stream);
+    checkError(error);
+
+
+    error = Pa_CloseStream(stream);
+    checkError(error);
+
+    error = Pa_Terminate();
+    checkError(error);
+    return EXIT_SUCCESS;
+}
